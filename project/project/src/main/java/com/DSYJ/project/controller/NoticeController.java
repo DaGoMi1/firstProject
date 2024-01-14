c72689ba006a0df5b34a5a6aa0e8a075324f9068
dlekrja4523 <dlekrja4523@gmail.com>
2024-01-14 16:45:05 +0900

    feat: first commit
    - springboot 프로젝트 빌드

---
+++ project/project/src/main/java/com/DSYJ/project/controller/NoticeController.java
@@ -0,0 +1,19 @@
+package com.DSYJ.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NoticeController {
+
+    @GetMapping("/notice")
+    public String notice() {
+        return "notice";
+    }
+
+    @GetMapping("/write")
+    public String write(){
+        return "write";
+    }
+
+}
